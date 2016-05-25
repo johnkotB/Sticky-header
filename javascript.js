6e6118c2ef1b410c1ebb7ce437bac3300c85be74
johnkotB <badykos@gmail.com>
2016-05-25 11:28:09 +0300

    demo

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
 ﻿$(document).ready(function () {
 
+    debugger;
+
     function stickyHeader(header, currentPos) {
-
         window.addEventListener('scroll', function (e) {
+            var distanceY = window.pageYOffset || document.documentElement.scrollTop;
 
-            var scrollDistanceY = window.pageYOffset || document.documentElement.scrollTop;
-
-            if (scrollDistanceY > currentPos) {
+            if (distanceY > currentPos) {
                 header.classList.add("sticky");
             }
             else {
@@ -20,6 +20,33 @@
 
     var header2 = document.getElementById('head');
     stickyHeader(header2, header2.offsetTop);
+
+
+    //function stickyHeader() {
+    //    window.addEventListener('scroll', function (e) {
+    //        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+    //        var header = document.querySelector(".head");
+    //        var shrinkOn = header.clientHeight;
+    //        if (distanceY > shrinkOn) {
+    //            header.classList.add("sticky");
+    //        }
+    //        else {
+    //            if (header.classList.contains("sticky")) {
+    //                header.classList.remove("sticky");
+    //            }
+    //        }
+    //    });
+    //}
+    //stickyHeader();
+
+
+
+    //var header1 = document.querySelector('.head[data-header]');
+    //var origOffsetY = 1;
+    //function onScroll(e) {
+    //    window.scrollY >= origOffsetY ? header1.classList.add('sticky') : header1.classList.remove('sticky');
+    //};
+    //document.addEventListener('scroll', onScroll);
 });
 
 

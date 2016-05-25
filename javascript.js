c1abeb768b4910750b9bdb6fde29ef5124eb4a84
johnkotB <badykos@gmail.com>
2016-05-25 10:34:01 +0300

    demo

---
+++ javascript.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    var header1 = document.querySelector('.head[data-header]');
+    //var header2 = document.getElementById('head');
+    var origOffsetY = 1;
+    //var offSetY = header2.offsetTop;
+
+    function onScroll(e) {
+        window.scrollY >= origOffsetY ? header1.classList.add('sticky') : header1.classList.remove('sticky');
+
+    };
+
+    document.addEventListener('scroll', onScroll);
+
+});
+
+

400410f5ea2a5112331c66792ea7f89a44e95c3b
Bogdan <badykos@gmail.com>
2016-07-04 12:00:15 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -21,32 +21,6 @@
     var header2 = document.getElementById('head');
     stickyHeader(header2, header2.offsetTop);
 
-
-    //function stickyHeader() {
-    //    window.addEventListener('scroll', function (e) {
-    //        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
-    //        var header = document.querySelector(".head");
-    //        var shrinkOn = header.clientHeight;
-    //        if (distanceY > shrinkOn) {
-    //            header.classList.add("sticky");
-    //        }
-    //        else {
-    //            if (header.classList.contains("sticky")) {
-    //                header.classList.remove("sticky");
-    //            }
-    //        }
-    //    });
-    //}
-    //stickyHeader();
-
-
-
-    //var header1 = document.querySelector('.head[data-header]');
-    //var origOffsetY = 1;
-    //function onScroll(e) {
-    //    window.scrollY >= origOffsetY ? header1.classList.add('sticky') : header1.classList.remove('sticky');
-    //};
-    //document.addEventListener('scroll', onScroll);
 });
 
 
